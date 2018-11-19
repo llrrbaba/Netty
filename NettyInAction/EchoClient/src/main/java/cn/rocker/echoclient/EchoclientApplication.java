fdcb08d4f9fdc8a776f1a1f4a5acf09472c9c88b
llrrbaba <rocker_pg@163.com>
2018-11-19 11:03:46 +0800

    echo client

---
+++ NettyInAction/EchoClient/src/main/java/cn/rocker/echoclient/EchoclientApplication.java
@@ -0,0 +1,12 @@
+package cn.rocker.echoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchoclientApplication.class, args);
+    }
+}
