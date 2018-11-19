5d690d3deb6aca07bbf503377739dc0f9742c0d8
llrrbaba <rocker_pg@163.com>
2018-11-19 10:08:57 +0800

    netty初次提交

---
+++ NettyDemo/src/main/java/cn/rocker/nettydemo/NettydemoApplication.java
@@ -0,0 +1,12 @@
+package cn.rocker.nettydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettydemoApplication.class, args);
+    }
+}
