ae75cc5a5295f3278a9b6efda642cda6b5c7db2d
llrrbaba <rocker_pg@163.com>
2018-11-19 11:02:42 +0800

    echo client

---
+++ NettyInAction/EchoClient/src/main/java/cn/rocker/echoclient/echo/EchoClientHandler.java
@@ -0,0 +1,37 @@
+package cn.rocker.echoclient.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author rocker
+ * @date 2018/11/19 10:30
+ * @since V1.0
+ */
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    Logger logger = LoggerFactory.getLogger(EchoServer.class);
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!",
+                CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, ByteBuf in) {
+        logger.info("Client received:{}", in.toString(CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx,
+                                Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
