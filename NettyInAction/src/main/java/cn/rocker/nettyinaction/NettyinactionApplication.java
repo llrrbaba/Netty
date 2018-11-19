f1bd4e37fa48b1dfca68141bf75d8b7ffe5201b6
llrrbaba <rocker_pg@163.com>
2018-11-19 10:05:13 +0800

    netty初次提交

---
+++ NettyInAction/src/main/java/cn/rocker/nettyinaction/NettyinactionApplication.java
@@ -0,0 +1,12 @@
+package cn.rocker.nettyinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyinactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyinactionApplication.class, args);
+    }
+}
---
+++ NettyInAction/src/main/java/cn/rocker/nettyinaction/echo/EchoServerHandler.java
@@ -0,0 +1,39 @@
+package cn.rocker.nettyinaction.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author rocker
+ * @date 2018/11/19 09:48
+ * @since V1.0
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    Logger logger = LoggerFactory.getLogger(EchoServerHandler.class);
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        logger.info("Server received:", in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
